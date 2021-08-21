df97966beb815de7ee2ee16eadf0ceaa34bed827
poison-pwn <45178461+poison-pwn@users.noreply.github.com>
2021-08-21 19:09:46 +0530

    changed css classes to kebab-case convention renamed griditem class to grid-square some formatting changes according to EsLint

--- etchjs.js
+++ etchjs.js
@@ -1,31 +1,32 @@
-const container = document.createElement('div');
-const main = document.querySelector('main');
+const container = document.createElement("div");
+const main = document.querySelector("main");
 
 main.appendChild(container);
-container.classList.add('gridcontainer');
+container.classList.add("grid-container");
 let noOfSquares = 8;
-frac=100/noOfSquares;
-container.style.setProperty('grid-template-columns',`repeat(${noOfSquares},${frac}%)`);
-container.style.setProperty('grid-template-rows',`repeat(${noOfSquares},${frac}%)`);
-let counter=1;
+frac = 100 / noOfSquares;
+container.style.setProperty(
+  "grid-template-columns",
+  `repeat(${noOfSquares},${frac}%)`
+);
+container.style.setProperty(
+  "grid-template-rows",
+  `repeat(${noOfSquares},${frac}%)`
+);
+let counter = 1;
 
 let item;
-while(counter<(noOfSquares**2)+1){
-     
-    item = document.createElement('div');
-    container.appendChild(item);
-    item.classList.add('griditem');
-    counter++;
+while (counter < noOfSquares ** 2 + 1) {
+  item = document.createElement("div");
+  container.appendChild(item);
+  item.classList.add("grid-square");
+  counter++;
 }
 
-const griditem = document.getElementsByClassName('griditem');
-for (var i = 0 ; i < griditem.length; i++) {
-    griditem[i].addEventListener('mouseover',(event)=>{
-        console.log('hi')
-        event.target.style.setProperty('background','blue');
-    })
- }
-
-
-
-
+const griditem = document.getElementsByClassName("grid-square");
+for (var i = 0; i < griditem.length; i++) {
+  griditem[i].addEventListener("mouseover", (event) => {
+    console.log("hi");
+    event.target.style.setProperty("background", "blue");
+  });
+}
