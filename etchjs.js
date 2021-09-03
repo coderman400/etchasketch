9192d647afba92a1217c7a49c12e5ad1d9c8747b
coderman400 <achurox1121@gmail.com>
2021-09-03 21:09:24 +0530

    add some small changes by neon, fix some weird bugs that popped up, declare the ishover variable which i didnt do before i have no idea how it was working before this lmao like i didnt even declare the variable but it still worked lol

--- etchjs.js
+++ etchjs.js
@@ -1,13 +1,13 @@
-const container = document.createElement('div');
-const main = document.querySelector('main');
+const container = document.createElement("div");
+const main = document.querySelector("main");
 main.appendChild(container);
-container.classList.add('gridcontainer');
+container.classList.add("gridcontainer");
 
 
 
 //slider
-const rightmenu = document.createElement('div');
-const slider = document.createElement('input');
+const rightmenu = document.createElement("div");
+const slider = document.createElement("input");
 main.appendChild(rightmenu);
 rightmenu.appendChild(slider);
 rightmenu.classList.add("rightmenu")
@@ -19,10 +19,10 @@ slider.value="8";
 
 
 //decide the grid dimensions
-let noOfSquares = 8;
+let squareSideLength = 8;
 
-const value = document.createElement('h1');
-value.classList.add('value');
+const value = document.createElement("h1");
+value.classList.add("value");
 rightmenu.appendChild(value);
 
 while(container.firstChild){
@@ -31,39 +31,40 @@ while(container.firstChild){
 
 
 
-let colorwheel = document.getElementsByClassName('colorwheel');
+let colorwheel = document.getElementsByClassName("colorwheel");
 colorChosen=colorwheel[0].value;
 
 colorwheel[0].addEventListener('change',(event)=>{
     colorChosen=colorwheel[0].value;
 })
 
-
+let isHover;
 
 //make an initial grid=================================
 
-frac=100/noOfSquares;
-container.style.setProperty('grid-template-columns',`repeat(${noOfSquares},${frac}%)`);
-container.style.setProperty('grid-template-rows',`repeat(${noOfSquares},${frac}%)`);
+frac=100/squareSideLength;
+container.style.setProperty('grid-template-columns',`repeat(${squareSideLength},${frac}%)`);
+container.style.setProperty('grid-template-rows',`repeat(${squareSideLength},${frac}%)`);
 
 let counter=1;
     let item;
-    while(counter<(noOfSquares**2)+1){
+    while(counter<(squareSideLength**2)+1){
         
         item = document.createElement('div');
         container.appendChild(item);
-        item.classList.add('griditem');
+        item.classList.add('grid-square');
         counter++;
 }
-value.textContent=`${noOfSquares}x${noOfSquares}`;
+value.textContent=`${squareSideLength}x${squareSideLength}`;
 //initial grid made =================================
 
 const checkHover = document.querySelector("#switch")
+console.log("this is checkhover")
 console.log(checkHover);
 
 checkHover.addEventListener('input',()=>{
     isHover=checkHover.checked;
-    console.log(checkHover.checked)
+    console.log(checkHover.checked);
 })
 
 //==============CREATE A GRID WHENEVER SLIDER VALUE IS ADJUSTED=================================
@@ -73,14 +74,14 @@ checkHover.addEventListener('input',()=>{
 slider.oninput = function() {
     
     console.log("EYY BREAD")
-    noOfSquares = this.value;
+    squareSideLength = this.value;
     //display dimensions
-    value.textContent=`${noOfSquares}x${noOfSquares}`;
+    value.textContent=`${squareSideLength}x${squareSideLength}`;
 
     //flexibility of grid to adjust size of individual squares according to dimensions of the grid
-    frac=100/noOfSquares;
-    container.style.setProperty('grid-template-columns',`repeat(${noOfSquares},${frac}%)`);
-    container.style.setProperty('grid-template-rows',`repeat(${noOfSquares},${frac}%)`);
+    frac=100/squareSideLength;
+    container.style.setProperty('grid-template-columns',`repeat(${squareSideLength},${frac}%)`);
+    container.style.setProperty('grid-template-rows',`repeat(${squareSideLength},${frac}%)`);
 
     //clear grid
     while(container.firstChild){
@@ -90,21 +91,18 @@ slider.oninput = function() {
     //creating n number of grid items and appending to the main container, this makes the grid 
     let counter=1;
     let item;
-    while(counter<(noOfSquares**2)+1){
+    while(counter<(squareSideLength**2)+1){
         
         item = document.createElement('div');
         container.appendChild(item);
-        item.classList.add('griditem');
+        item.classList.add('grid-square');
         counter++;
     }
 
     //on hovering, we change the color of the griditem
-const griditem = document.getElementsByClassName('griditem');
-
-
-for (var i = 0 ; i < griditem.length; i++) {
-    
-    griditem[i].addEventListener('mouseover',(event)=>{
+    let gridSquares = document.getElementsByClassName("grid-square");
+    for (gridSquare of gridSquares) {
+      gridSquare.addEventListener("mouseover", (event) => {
         if(isHover==true){
         console.log('hi')
         event.target.style.setProperty('background',colorChosen);
@@ -112,13 +110,15 @@ for (var i = 0 ; i < griditem.length; i++) {
         }
     })
  }
-}
-const griditem = document.getElementsByClassName('griditem');
+};
 
 //on hovering, we change the color of the griditem
 
-for (var i = 0 ; i < griditem.length; i++) {
-    griditem[i].addEventListener('mouseover',(event)=>{
+let gridSquares = document.getElementsByClassName("grid-square");
+console.log("yo this is gridsquares");
+console.log(gridSquares);
+for (gridSquare of gridSquares) {
+  gridSquare.addEventListener("mouseover", (event) => {
         if(isHover==true){
         console.log('hi')
         event.target.style.setProperty('background',colorChosen);
@@ -130,8 +130,8 @@ for (var i = 0 ; i < griditem.length; i++) {
  //clear button on click clears the grid back to white
  const clearButton = document.getElementById('clear')
  clearButton.addEventListener('click',()=>{
-     for (var i = 0 ; i < griditem.length; i++) {
-            griditem[i].style.setProperty('background','white');
+     for (var i = 0 ; i < gridSquares.length; i++) {
+            gridSquares[i].style.setProperty('background','white');
      }
           
 })
